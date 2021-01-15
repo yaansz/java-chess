7f572937394fbbefa1d7f88dd8784b39f3d90e3c
Yan Figueiredo <yancfm@gmail.com>
2021-01-15 15:31:26 -0300

    Adding ChessException and a conversor between matrix and chess positions

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
-import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -17,12 +15,23 @@ public class ChessMatch {
 	
 	/**
 	 * 
+	 * Place a new piece in board by the ChessPosition Logic.
+	 * 
+	 * @param column
+	 * @param row
+	 * @param piece
+	 */
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	/**
+	 * 
 	 * Method to arranging chess pieces in their respective places.
 	 * 
 	 */
 	private void InitialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(2, 1));
+		this.placeNewPiece('b', 4, new Rook(board, Color.WHITE));
 	}
 	
 	/**
